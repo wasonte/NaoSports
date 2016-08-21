038c9b06c19d18b985a26b83cd07cf788bc045f1
George <wasonte@gmail.com>
2016-08-22 00:28:59 +0200

    Connect activity in progress

---
+++ naosports/src/main/java/tfg/jorgealcolea/naosports/ConnectActivity.java
@@ -0,0 +1,17 @@
+package tfg.jorgealcolea.naosports;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by george on 21/08/16.
+ */
+public class ConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+    }
+}
